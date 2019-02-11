cce6332f817a3bc92f3377cd395778107c83afc4
Steffen <steffen@robots.ox.ac.uk>
2019-02-11 17:21:35 +0000

    First Day

--- src/position_controller_cdt/src/position_controller_cdt/position_controller_cdt.cpp
+++ src/position_controller_cdt/src/position_controller_cdt/position_controller_cdt.cpp
@@ -1,10 +1,11 @@
 #include "position_controller_cdt/position_controller_cdt.hpp"
+#include "math.h"
 
 PositionController::PositionController(){
   std::cout << "Finished setting up PositionController\n";
 }
 
-
+// takes Quaterniod coordinates, and writes the passed-by-reference roll, pitch and yaw
 void PositionController::quat_to_euler(Eigen::Quaterniond q, double& roll, double& pitch, double& yaw) {
   const double q0 = q.w();
   const double q1 = q.x();
@@ -29,12 +30,15 @@ FOLLOWER_OUTPUT PositionController::computeControlCommand(Eigen::Isometry3d curr
   double linear_forward_x = 0;
   double linear_forward_y = 0;
   double angular_velocity = 0;
+
+  // static double integral = 0;
   // Develop your controller here within the calls
 
   // EXAMPLE HEADING CONTROLLER CODE - ADD YOUR OWN POSITION + HEADING CONTROLLER HERE
   ///////////////////////////////////////////////////////////////////////
   ///////////////////////////////////////////////////////////////////////
 
+  // convert own orientation to Euler coords
   Eigen::Quaterniond q(current_pose.rotation());
   double current_roll, current_pitch, current_yaw;
   quat_to_euler(q, current_roll, current_pitch, current_yaw);
@@ -43,11 +47,42 @@ FOLLOWER_OUTPUT PositionController::computeControlCommand(Eigen::Isometry3d curr
   double goal_roll, goal_pitch, goal_yaw;
   quat_to_euler(q_goal, goal_roll, goal_pitch, goal_yaw);
 
+  //Eigen::Translation x(current_pose.translation());
+  //Eigen::Translation x_goal(current_goal_.translation());
+  //x.x()
+
+  double x_pos = current_pose.translation().x();
+  double y_pos = current_pose.translation().y();
+
+  double x_currentgoal = current_goal_.translation().x();
+  double y_currentgoal = current_goal_.translation().y();
+  
+
+
+
   // compute the P control output:
   double headingErrorRaw = current_yaw - goal_yaw;
   double headingError = constrainAngle(headingErrorRaw);
-  double angular_gain_p_ = 10;
+
+  //TODO needs tuning
+  double angular_gain_p_ = 0.1;
   angular_velocity = -headingError * angular_gain_p_;
+
+  // positional control
+  double x_error =  (cos(x_currentgoal) + sin(y_currentgoal) - (cos(x_pos) + sin(y_pos));
+  double y_error =  (-sin(x_currentgoal) + cos(y_currentgoal) - (-sin(x_pos) + cos(y_pos)) ;
+
+  //double pos_error = sqrt( pow(x_error, 2.0) + pow(y_error, 2.0));
+
+  double forward_gain = 1;
+  double strafe_gain = 0.5; 
+  //double rel_vel = forward_gain * pos_error;
+
+  forward_speed = forward_gain * x_error;
+  strafe_speed = strafe_gain * y_error;
+
+  linear_forward_x = cos(current_yaw) * rel_vel;
+  linear_forward_y = sin(current_yaw) * rel_vel;
 
   std::cout << "current_yaw: " << current_yaw << ", raw error: " << headingErrorRaw
             << ", constrained error: " << headingError << ", des ang vel: " << angular_velocity << std::endl;
